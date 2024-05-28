f6cf15314bdf874b663ecc4864294135c1d1f08f
A1RONE <125598830+A1RONE@users.noreply.github.com>
2024-05-28 10:23:42 +0200

    commentaires sur la stateMachine

--- src/state_machine/State.java
+++ src/state_machine/State.java
@@ -22,23 +22,42 @@ public abstract class State {
 
     // Definie l'ensemble des executions à l'entrée de l'etat
     // A définir dans la classe fille
+    /**
+     * Methode qui execute les actions correspondant à l'entrée dans l'etat
+     */
     public void Enter(){}
     // Definie l'ensemble des executions à la sortie de l'état
     // A définir dans la classe fille
+    /**
+     * Methode qui execute les actions correspondant à la sortie de l'etat
+     */
     public void Exit(){}
     // Definie l'ensemble des exécutions à chaque Update de l'état
     // A définir dans la classe fille
+    /**
+     * Methode qui execute les actions correspondant à l'update de l'etat
+     */
     public void Update(){}
 
     // Getter permettant de vérifier si on exécute une transition
+    /**
+     * Getter de la transition
+     */
     public boolean isTransition() {
         return m_transition;
     }
     // Getter permettant de récupérer le nom de la classe
+    /**
+     * Getter du nom
+     */
     public String get_name(){ return m_name; }
 
     // Methode permettant d'obtenir le nom de la classe vers laquelle on transition
     // On stop la transition avec "m_transition = false;"
+    /**
+     * Methode qui retourne le nom de l'etat vers lequel on effectue la transition
+     * et met le boolean de la transition a false.
+     */
     public String get_transition(){
         m_transition = false;
         return m_transition_name;
--- src/state_machine/StateMachine.java
+++ src/state_machine/StateMachine.java
@@ -2,23 +2,64 @@ import java.util.Map;
 
 public class StateMachine {
 
+    /*
+
+    Classe dont le rôle est de réaliser des transitions entre les etats des objets.
+
+    ready() : permet d'initialiser le premier etat
+
+    Update() : permet d'effectuer l'update de la classe actuelle et de verifier si il y a une transition
+
+    child_transition() : permet de realiser la transition entre différents état en
+        appelant Exit() sur le current
+        changant le current par le new
+        appelant Enter() sur le new
+
+     */
+
+    // Map comprenant l'ensemble des etat ainsi que les String permettant de les appeler
     private Map<String,State> m_states;
+    // State definissant l'etat actuel (celui sur lequel Update est appeler)
     private State m_current_state;
 
+    // Appelle le constructeur de la StateMachine en definissant un premier current_state et la map comprenant tout les etats
+    // La Map est à définire dans le constructeur
+    /**
+     * Constructeur de classe avec un Hashmap de String et de State
+     * @param initial_state reference le nom de l'etat initial : String
+     * @param states reference la Hashmap : Map<String,State>
+     */
     public StateMachine(String initial_state, Map<String,State> states ){
         m_states = states;
         ready(m_states.get(initial_state));
     }
 
-    public void ready(State state){
+    // Methode appelée par le constructeur pour initialiser l'etat courrant
+    /**
+     * Methode qui initialise l'etat courrant
+     * @param state reference l'etat initiale : State
+     */
+    private void ready(State state){
         m_current_state = state;
         m_current_state.Enter();
     }
 
+    // Getter permettant de recuperer l'etat actuel afin de pouvoir recuperer les données
+    // voulues par l'entite
+    /**
+     * Getter de l'etat actuel
+     */
     public State getState(){
         return m_current_state;
     }
 
+    // Methode permettant d'appeler l'update de l'etat
+    // Si l'etat effectue uen transition alors on appele :
+    // child_transition( nom de l'etat actuel, nom de l'etat suivant)
+    // Qui permet d'effectuer la transition
+    /**
+     * Methode qui Update l'etat actuel
+     */
     public void Update(){
 
         m_current_state.Update();
@@ -27,7 +68,19 @@ public class StateMachine {
         }
     };
 
+    // Méthode permettant d'effectuer une transition à l'aide du nom de l'etat courrant
+    // et du nom de l'etat vers lequel on transitionne
+    // appelle Exit
+    // realise la transition (changement de m_current_state)
+    // appelle Enter
+    /**
+     * Methode qui réalise la transition d'etat
+     * @param state reference le nom de l'etat actuel
+     * @param new_state reference le nom du nouvel etat
+     */
     public void child_transition(String state, String new_state){
+
+        // verifie si on transitionne bien de l'etat actuel vers un nouvel etat
         if (m_current_state != m_states.get(state) ||  state == new_state ) { // A rajouter verifier que new_state est bien compris dans l'enum
             System.out.println("StateMachine error");
             return;
